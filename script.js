5c64d8324129912c1d6f462ba15b12c8dc1fc324
nghtudung <nghtudung@gmail.com>
2024-07-19 22:34:03 +0700

    about us

---
+++ script.js
@@ -0,0 +1,11 @@
+const em = document.querySelector('.em');
+const classname = document.querySelector('.classname');
+const header = document.querySelector('.header');
+em.addEventListener('animationend', ()=>{
+    classname.style.opacity = 1;
+    classname.classList.add('animate__animated','animate__zoomInDown');
+});
+classname.addEventListener('animationend', ()=>{
+    header.style.opacity = 1;
+    header.classList.add('animate__animated','animate__slideInDown');
+});
